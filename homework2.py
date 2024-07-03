e73e141601bff56b2d11771443ce314d4d34bda2
JDH-993 <sonya.shlyakhova@mail.ru>
2024-07-03 10:32:52 +0700

    Add files via upload

---
+++ homework2.py
@@ -0,0 +1,5 @@
+kolvoHomeworks = 12
+kolvoHours = 1.5
+nameOfKurs = 'Python'
+TimeFor1Work = 1.5/12
+print("Курс:",nameOfKurs, ", всего задач:", kolvoHomeworks, ", затрачено часов:", kolvoHours, ", среднее время выполнения", TimeFor1Work,   "часа.")
